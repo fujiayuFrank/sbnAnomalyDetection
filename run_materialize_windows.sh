2f152e4feec5c58d315704c7f43a7236aadaf4cd
Jiayu Fu <jiayufu@sbndgpvm01.fnal.gov>
2026-06-17 09:53:44 -0500

    running train and test npz

--- run_materialize_windows.sh
+++ run_materialize_windows.sh
@@ -4,7 +4,13 @@
 # modify for single file testing
 # --root-files root://fndcadoor.fnal.gov//pnfs/fs/usr/sbnd/persistent/users/micarrig/DQM//CI_build_lar_ci_19305/reco/reco/DQMValidationTrees_00.root root://fndcadoor.fnal.gov//pnfs/fs/usr/sbnd/persistent/users/micarrig/DQM//CI_build_lar_ci_19305/reco/reco/DQMValidationTrees_01.root\
 
+# running training data materialization
+# --root-file-list data/filelist_train.txt \
+
+# running testing data materialization
+# --root-file-list data/filelist_test.txt \
+
 python -m sbn_anomaly.data.materialize_windows \
   --config configs/materialize_windows.yaml \
-  --root-file-list sbn_anomaly/data/filelist.txt \
-  --output good_bad.npy
+  --root-file-list data/filelist.txt \
+  --output testing.npz
